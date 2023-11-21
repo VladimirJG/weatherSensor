a6cf1cee0dd4822909c082467467f85ef94d8f15
VladimirJG <vladimi85@gmail.com>
2023-11-21 16:08:48 +0300

    added and modified Measurement Controller, Service, Response;

---
+++ src/main/java/ru/danilov/rest/weatherSensor/controllers/MeasurementController.java
@@ -0,0 +1,72 @@
+package ru.danilov.rest.weatherSensor.controllers;
+
+import jakarta.validation.Valid;
+import org.modelmapper.ModelMapper;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+import ru.danilov.rest.weatherSensor.dto.MeasurementDTO;
+import ru.danilov.rest.weatherSensor.dto.MeasurementResponse;
+import ru.danilov.rest.weatherSensor.exceptions.ErrorResponse;
+import ru.danilov.rest.weatherSensor.exceptions.MeasurementException;
+import ru.danilov.rest.weatherSensor.model.Measurement;
+import ru.danilov.rest.weatherSensor.services.MeasurementService;
+import ru.danilov.rest.weatherSensor.util.MeasurementValidator;
+
+import java.util.stream.Collectors;
+
+import static ru.danilov.rest.weatherSensor.util.ErrorsUtil.returnErrorsToClient;
+
+@RestController
+@RequestMapping("/measurement")
+public class MeasurementController {
+    private final MeasurementService measurementService;
+    private final MeasurementValidator measurementValidator;
+    private final ModelMapper modelMapper;
+
+    public MeasurementController(MeasurementService measurementService, MeasurementValidator measurementValidator, ModelMapper modelMapper) {
+        this.measurementService = measurementService;
+        this.measurementValidator = measurementValidator;
+        this.modelMapper = modelMapper;
+    }
+
+    @GetMapping()
+    public MeasurementResponse getAll() {
+        return new MeasurementResponse(measurementService.getAllMeasurements().stream()
+                .map(this::convertToMeasurementDTO).collect(Collectors.toList()));
+    }
+
+    @PatchMapping("/add")
+    public ResponseEntity<HttpStatus> add(@RequestBody @Valid MeasurementDTO measurementDTO, BindingResult bindingResult) {
+        Measurement measurementToAdd = convertToMeasurement(measurementDTO);
+        measurementValidator.validate(measurementToAdd, bindingResult);
+        if (bindingResult.hasErrors()) {
+            returnErrorsToClient(bindingResult);
+        }
+        measurementService.saveMeasurement(measurementToAdd);
+        return ResponseEntity.ok(HttpStatus.OK);
+    }
+
+    @GetMapping("/rainyDaysCount")
+    public Long getRainyDaysCount() {
+        return measurementService.getAllMeasurements().stream().filter(Measurement::isRaining).count();
+    }
+
+    private Measurement convertToMeasurement(MeasurementDTO measurementDTO) {
+        return modelMapper.map(measurementDTO, Measurement.class);
+    }
+
+    private MeasurementDTO convertToMeasurementDTO(Measurement measurement) {
+        return modelMapper.map(measurement, MeasurementDTO.class);
+    }
+
+    @ExceptionHandler
+    private ResponseEntity<ErrorResponse> handelException(MeasurementException exception) {
+        ErrorResponse response = new ErrorResponse(
+                exception.getMessage(),
+                System.currentTimeMillis()
+        );
+        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+    }
+}
--- src/main/java/ru/danilov/rest/weatherSensor/services/MeasurementService.java
+++ src/main/java/ru/danilov/rest/weatherSensor/services/MeasurementService.java
@@ -3,9 +3,7 @@ package ru.danilov.rest.weatherSensor.services;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import ru.danilov.rest.weatherSensor.exceptions.SensorNotFoundException;
 import ru.danilov.rest.weatherSensor.model.Measurement;
-import ru.danilov.rest.weatherSensor.model.Sensor;
 import ru.danilov.rest.weatherSensor.repositories.MeasurementRepository;
 
 import java.time.LocalDateTime;
@@ -16,41 +14,27 @@ import java.util.Optional;
 @Transactional(readOnly = true)
 public class MeasurementService {
     private final MeasurementRepository measurementRepository;
+    private final SensorService sensorService;
 
     @Autowired
-    public MeasurementService(MeasurementRepository measurementRepository) {
+    public MeasurementService(MeasurementRepository measurementRepository, SensorService sensorService) {
         this.measurementRepository = measurementRepository;
+        this.sensorService = sensorService;
     }
 
     public List<Measurement> getAllMeasurements() {
         return measurementRepository.findAll();
     }
 
-    public Optional<Measurement> getMeasurementById(int id) {
-        return measurementRepository.findById(id);
-    }
 
     @Transactional
     public void saveMeasurement(Measurement measurement) {
-        measurement.setTimeOfCreation(LocalDateTime.now());
+        enrichMeasurement(measurement);
         measurementRepository.save(measurement);
     }
 
-    @Transactional
-    public void updateMeasurement(Measurement upMeasurement, int id) {
-        Optional<Measurement> measurement = measurementRepository.findById(id);
-
-        upMeasurement.setSensor(measurement.get().getSensor());
-        upMeasurement.setId(measurement.get().getId());
-        measurementRepository.save(upMeasurement);
-    }
-
-    @Transactional
-    public void deleteMeasurement(int id) {
-        measurementRepository.deleteById(id);
-    }
-
-    public Sensor getSensorOwner(int id) {
-        return measurementRepository.findById(id).map(Measurement::getSensor).orElse(null);
+    private void enrichMeasurement(Measurement measurement) {
+        measurement.setSensor(sensorService.getByName(measurement.getSensor().getName()).get());
+        measurement.setTimeOfCreation(LocalDateTime.now());
     }
 }
